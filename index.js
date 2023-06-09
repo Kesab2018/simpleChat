8a4a2fb81ba3f442719db3dfafe4dfba62753544
Kesab Nayak <kesab.nayak@stud.th-deg.de>
2023-06-09 13:09:38 +0200

    working on it

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.get('/',(req,res) => res.sendFile(__dirname + '/index.html'))
 
 io.on('connection', (socket) => {
     
-    socket.on('message', (msg) => io.emit('message',msg))
+    socket.on('message', (msg) => io.emit('message',{'user': socket.username, 'message': msg}))
     socket.on('join', (username) => {
         if(username != null){
             socket.username = username
         }
-        socket.broadcast.emit('message', 'some one joined')
+        socket.broadcast.emit('message', {user: 'Server', 'message': socket.username + ' has joined'})
     })
 });
 http.listen(3000, () => console.log('Listening on port 3000'))
